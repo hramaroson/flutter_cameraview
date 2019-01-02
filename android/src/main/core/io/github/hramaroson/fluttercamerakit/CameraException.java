91bbe2aa16ae850d0e978e5e58799b92c9a97978
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2019-01-02 22:56:33 +0300

    Use natario1 's  CameraView as main widget (Android)

--- android/src/main/core/io/github/hramaroson/fluttercamerakit/CameraException.java
+++
@@ -1,7 +0,0 @@
-package io.github.hramaroson.fluttercamerakit;
-
-public class CameraException extends RuntimeException{
-    public CameraException (Throwable cause){
-        super(cause);
-    }
-}
