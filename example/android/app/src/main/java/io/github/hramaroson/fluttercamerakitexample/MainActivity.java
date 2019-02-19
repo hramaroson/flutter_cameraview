21b8c52950703e52435ebb4d29251f94622923c1
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2019-02-19 18:24:03 +0300

    Update example: Set app orientation to Portrait (Android)

--- example/android/app/src/main/java/io/github/hramaroson/fluttercamerakitexample/MainActivity.java
+++ example/android/app/src/main/java/io/github/hramaroson/fluttercamerakitexample/MainActivity.java
@@ -3,11 +3,14 @@ package io.github.hramaroson.fluttercamerakitexample;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import android.content.pm.ActivityInfo;
 
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
   }
 }
