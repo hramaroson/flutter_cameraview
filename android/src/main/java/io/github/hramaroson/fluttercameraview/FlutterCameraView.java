62fdeb03ec7cfc8af5c97f0e61543cc4d1c05972
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2019-03-04 01:48:38 +0300

    Save Image file when takePicture is called (Android)

--- android/src/main/java/io/github/hramaroson/fluttercameraview/FlutterCameraView.java
+++ android/src/main/java/io/github/hramaroson/fluttercameraview/FlutterCameraView.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.PictureResult;
+import com.otaliastudios.cameraview.FileCallback;
 import com.otaliastudios.cameraview.Audio;
 import com.otaliastudios.cameraview.Flash;
 import com.otaliastudios.cameraview.Facing;
+import com.otaliastudios.cameraview.Mode;
 
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
@@ -17,20 +21,32 @@ import io.flutter.plugin.common.MethodChannel;
 import static io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.platform.PlatformView;
 
-public class FlutterCameraView implements PlatformView, MethodCallHandler, Application.ActivityLifecycleCallbacks {
+import java.io.File;
+import androidx.annotation.Nullable;
+import androidx.annotation.UiThread;
+
+public class FlutterCameraView implements PlatformView, MethodCallHandler, 
+        Application.ActivityLifecycleCallbacks {
     private final CameraView mCameraView;
     private final MethodChannel mMethodChanel;
     private final Context mContext;
 
+    private String mCapturedImagePath;
+
     FlutterCameraView(Context context, BinaryMessenger messenger, int id, Activity activity){
         mContext = context;
         mCameraView = new CameraView(context);
-        mCameraView.setAudio(Audio.OFF);
         mMethodChanel = new MethodChannel(messenger, "plugins.hramaroson.github.io/cameraview_" + id);
         mMethodChanel.setMethodCallHandler(this);
 
         activity.getApplication().registerActivityLifecycleCallbacks(this);
+        
         mCameraView.open();
+        mCameraView.setMode(Mode.PICTURE);
+        mCameraView.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(PictureResult result) { onPicture(result);}
+        });
     }
 
     @Override
@@ -170,7 +186,9 @@ public class FlutterCameraView implements PlatformView, MethodCallHandler, Appli
     }
 
     private void setFlash(MethodCall methodCall, MethodChannel.Result result){
-        mCameraView.setFlash(__flashValueFromIndex((int) methodCall.arguments));
+        if (!mCameraView.isTakingPicture() && !mCameraView.isTakingVideo()){
+            mCameraView.setFlash(__flashValueFromIndex((int) methodCall.arguments));
+        }
         result.success (null);
     }
     
@@ -179,7 +197,17 @@ public class FlutterCameraView implements PlatformView, MethodCallHandler, Appli
     }
 
     private void takePicture(MethodCall methodCall, MethodChannel.Result result) {
-        String filePath = (String) methodCall.arguments;
-        result.success (filePath);
+        mCapturedImagePath = (String) methodCall.arguments;
+
+        mCameraView.takePicture();
+        result.success (mCapturedImagePath);
+    }
+
+    private void onPicture(PictureResult result){
+        result.toFile(new File(mCapturedImagePath), new FileCallback() {
+            @UiThread
+            public void onFileReady(@Nullable File file){
+            }
+        });
     }
 }
